d8b89e5047bfd783d38c225b3678bed2e48230b0
pangolin <hub500@protonmail.com>
2025-09-09 18:43:45 +0800

    Create eva-eth-sync.sh

---
+++ eva-eth-sync.sh
@@ -0,0 +1,6 @@
+
+curl -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_syncing","params":[],"id":1}' http://18.163.81.118:8545
+
+curl -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_syncing","params":[],"id":1}' http://16.163.97.13:8545
+
+curl -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_syncing","params":[],"id":1}' http://43.198.212.114:8545
