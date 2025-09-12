bb1aedbe46d8ac415cdf84bd4d07ddbb4cb5d2c7
pangolin <hub500@protonmail.com>
2025-09-12 12:27:21 +0800

    genesis

---
+++ network-configs/genesis.sh
@@ -0,0 +1,6 @@
+eth-genesis-state-generator beaconchain \
+  --config config.yaml \
+  --eth1-config genesis.json \
+  --mnemonics mnemonics.yaml \
+  --state-output genesis.ssz \
+  --json-output parsedConsensusGenesis.json
