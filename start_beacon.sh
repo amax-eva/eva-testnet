2484d149d0a781e311762ef1a6d3635396fd2c5c
pangolin <hub500@protonmail.com>
2025-09-13 10:22:21 +0800

    beacon config

--- start_beacon.sh
+++ start_beacon.sh
@@ -2,36 +2,18 @@
 EXTIP=$(ip addr show ens5 | grep 'inet ' | awk '{print $2}' | cut -d'/' -f1)
 echo $EXTIP
 
+CHECKPOINT_URL="http://172.31.30.93:3500"
+FEE_RECIPIENT="0x6318BC08F350835f8b2e2A542f04e2aB129Ab5C4"
+
+
 nohup beacon-chain \
-  --chain-id=2248 \
-  --network-id=2248 \
-  --datadir=$PWD/prysm/beacon-data \
-  --execution-endpoint=http://127.0.0.1:8551 \
-  --chain-config-file=$PWD/network-configs/config.yaml \
-  --genesis-state=$PWD/network-configs/genesis.ssz \
-  --jwt-secret=$PWD/jwt.hex \
-  --rpc-host=0.0.0.0 \
-  --rpc-port=4000 \
-  --http-host=0.0.0.0 \
-  --http-cors-domain=* \
-  --http-port=3500 \
+  --config-file=$PWD/config/beacon.yaml \
+  --datadir=$PWD/data/beacon \
+  --checkpoint-sync-url=$CHECKPOINT_URL \
   --p2p-host-ip=$EXTIP \
-  --p2p-tcp-port=13000 \
-  --p2p-udp-port=12000 \
-  --p2p-quic-port=13000 \
-  --p2p-static-id=true \
-  --p2p-max-peers=50 \
-  --min-sync-peers=0 \
-  --verbosity=debug \
-  --slots-per-archive-point=32 \
-  --suggested-fee-recipient=0x6318BC08F350835f8b2e2A542f04e2aB129Ab5C4 \
-  --disable-monitoring=false \
-  --monitoring-host=0.0.0.0 \
-  --monitoring-port=8080 \
-  --accept-terms-of-use=true \
-  --no-discovery=true \
-  --pprof \
-  --pprofaddr=0.0.0.0 \
-  --pprofport=6060 \
-  --contract-deployment-block=0 \
+  --suggested-fee-recipient=$FEE_RECIPIENT \
+  --jwt-secret=$PWD/secret/jwt.hex \
+  --chain-config-file=$PWD/network/config.yaml \
+  --genesis-state=$PWD/network/genesis.ssz \
+  --log-file=$PWD/logs/beacon.log \
   > $PWD/beacon.log 2>&1 &
--- start_beacon2.sh
+++ start_beacon2.sh
@@ -2,43 +2,24 @@
 EXTIP=$(ip addr show ens5 | grep 'inet ' | awk '{print $2}' | cut -d'/' -f1)
 echo $EXTIP
 
-BOOTNODE=$(curl http://18.163.81.118:3500/eth/v1/node/identity | jq -r '.data.enr')
-echo $BOOTNODE
+# p2p发现关闭，获取不到ENR值
+# BOOTNODE=$(curl http://18.163.81.118:3500/eth/v1/node/identity | jq -r '.data.enr')
+# echo $BOOTNODE
+
+CHECKPOINT_URL="http://172.31.30.93:3500"
+FEE_RECIPIENT="0xF84AE3A59D9c8a08b9308Ba4D3d0341135c51989"
+PEER="/ip4/172.31.30.93/tcp/13000/p2p/16Uiu2HAmFyo8PZqPVq6yVHfSeSrojE2Y3wd3fkqMyF53me24SZ3P"
+
 
 nohup beacon-chain \
-  --chain-id=2248 \
-  --network-id=2248 \
-  --datadir=$PWD/prysm/beacon-data/ \
-  --execution-endpoint=http://127.0.0.1:8551 \
-  --checkpoint-sync-url=http://172.31.30.93:3500 \
-  --rpc-host=0.0.0.0 \
-  --rpc-port=4000 \
-  --http-host=0.0.0.0 \
-  --http-cors-domain=* \
-  --http-port=3500 \
+  --config-file=$PWD/config/beacon.yaml \
+  --datadir=$PWD/data/beacon \
+  --checkpoint-sync-url=$CHECKPOINT_URL \
   --p2p-host-ip=$EXTIP \
-  --p2p-tcp-port=13000 \
-  --p2p-udp-port=12000 \
-  --p2p-quic-port=13000 \
-  --min-sync-peers=0 \
-  --p2p-max-peers=50 \
-  --verbosity=debug \
-  --slots-per-archive-point=32 \
-  --suggested-fee-recipient=0x6318BC08F350835f8b2e2A542f04e2aB129Ab5C4 \
-  --jwt-secret=$PWD/jwt.hex \
-  --disable-monitoring=false \
-  --monitoring-host=0.0.0.0 \
-  --monitoring-port=8080 \
-  --accept-terms-of-use=true \
-  --no-discovery=true \
-  --pprof \
-  --pprofaddr=0.0.0.0 \
-  --pprofport=6060 \
-  --p2p-static-id=true \
-  --chain-config-file=$PWD/network-configs/config.yaml \
-  --genesis-state=$PWD/network-configs/genesis.ssz \
-  --contract-deployment-block=0 \
-  --peer="/ip4/172.31.30.93/tcp/13000/p2p/16Uiu2HAmFyo8PZqPVq6yVHfSeSrojE2Y3wd3fkqMyF53me24SZ3P" \
+  --suggested-fee-recipient=$FEE_RECIPIENT \
+  --jwt-secret=$PWD/secret/jwt.hex \
+  --chain-config-file=$PWD/network/config.yaml \
+  --genesis-state=$PWD/network/genesis.ssz \
+  --log-file=$PWD/logs/beacon.log \
+  --peer=$PEER \
   > $PWD/beacon.log 2>&1 &
-
-# --no-discovery=false \
