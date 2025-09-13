a059e460db8ef54060743db4535fc828f6d2b098
pangolin <hub500@protonmail.com>
2025-09-13 09:58:44 +0800

    erigon config

--- start_erigon2.sh
+++ start_erigon2.sh
@@ -5,27 +5,13 @@ echo $EXTIP
 BOOTNODE=$(curl -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"admin_nodeInfo","params":[],"id":1}' http://18.163.81.118:8545 | jq -r '.result.enode')
 echo $BOOTNODE
 
-nohup erigon --networkid=2248 \
-  --verbosity=debug \
-  --datadir=$PWD/erigon/execution-data \
-  --port=30303 \
-  --http.api=eth,erigon,engine,web3,net,debug,trace,txpool,admin \
-  --http.vhosts=* \
-  --ws \
-  --allow-insecure-unlock \
+# enode://b6b4b7bae96823c426b612e7d2080e4bcc5c8972d59f3fce983c2a327efc30571c8bc1b275182b54e08a9e2e639af8f35f1556b7a92bd2d6286840d03ef0659a@172.31.30.93:30303
+
+nohup erigon \
+  --config=$PWD/config/erigon.yaml \
+  --datadir=$PWD/data/erigon \
+  --log.dir.path=$PWD/logs \
   --nat=extip:$EXTIP \
-  --http \
-  --http.addr=0.0.0.0 \
-  --http.corsdomain=* \
-  --http.port=8545 \
-  --authrpc.jwtsecret=$PWD/jwt.hex \
-  --authrpc.addr=0.0.0.0 \
-  --authrpc.port=8551 \
-  --authrpc.vhosts=* \
-  --externalcl \
-  --metrics \
-  --metrics.addr=0.0.0.0 \
-  --metrics.port=9001 \
-  --torrent.port=42069 \
-  --bootnodes="enode://b6b4b7bae96823c426b612e7d2080e4bcc5c8972d59f3fce983c2a327efc30571c8bc1b275182b54e08a9e2e639af8f35f1556b7a92bd2d6286840d03ef0659a@172.31.30.93:30303" \
+  --authrpc.jwtsecret=$PWD/secret/jwt.hex \
+  --bootnodes=$BOOTNODE \
   > $PWD/erigon.log 2>&1 &
